5cee35a8102f6bafd8f16df2619016190eff182e
laurawebsterp <laura.pereira@rubcube.com>
2024-11-13 14:26:48 -0300

    Adicionando Knex

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,14 @@
+// routes/userRoutes.js
+const express = require("express");
+//const UserController = require("./src/controllers/UsuarioController");
+
+const router = express.Router();
+
+//router.post("/register", UserController.createUser); // Rota para criar um usuário
+//router.post("/login", UserController.login); // Rota para login
+//router.get("/", UserController.getAllUsers); // Rota para obter todos os usuários
+//router.get("/:id", UserController.getUserById); // Rota para obter um usuário por ID
+//router.put("/:id", UserController.updateUser); // Rota para atualizar um usuário
+//router.delete("/:id", UserController.deleteUser); // Rota para deletar um usuário
+
+module.exports = router;
